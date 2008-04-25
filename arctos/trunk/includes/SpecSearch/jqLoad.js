c89aa6ed7b7103faf01167669ecca4d2b4900fc6
dkthung <dkthung@de876385-3c43-0410-a6ee-4d0c202aae61>
2008-04-25 00:03:39 +0000

    Added cookies functions for saving specPref for users who did not logged in.

--- arctos/trunk/includes/SpecSearch/jqLoad.js
+++ arctos/trunk/includes/SpecSearch/jqLoad.js
@@ -134,7 +134,7 @@ function saveComplete(savedStr){
 			// else nothing needs to be done
 		}
 	
-		var nCookie = cookieArray.join(",");	
+		var nCookie = cookieArray.join(",");
 		createCookie("specsrchprefs", nCookie, 0);
 	}
 }
