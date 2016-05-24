879b9cb4b48d487b489ec903643fd2cfdd87e354
dustymc <dustymc@gmail.com>
2016-05-24 11:12:29 -0700

    Signed-off-by: dustymc <dustymc@gmail.com>

--- includes/wellknown.js
+++ includes/wellknown.js
@@ -1,9 +1,4 @@
 
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.wellknown = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*eslint-disable no-cond-assign */
-module.exports = parse;
-module.exports.parse = parse;
-module.exports.stringify = stringify;
 
 var numberRegexp = /[-+]?([0-9]*\.[0-9]+|[0-9]+)([eE][-+]?[0-9]+)?/;
 // Matches sequences like '100 100' or '100 100 100'.
@@ -276,7 +271,5 @@ function stringify (gj) {
   }
 }
 
-},{}]},{},[1])(1)
-});
 
   
