b49a847b0e6364530d21a00d897263909d512111
dustymc <dustymc@gmail.com>
2018-04-19 10:14:08 -0700

    .
    Signed-off-by: dustymc <dustymc@gmail.com>

--- includes/ajax.js
+++ includes/ajax.js
@@ -11,19 +11,19 @@ $(document).ready(function() {
 			var f=this.id;
 		}
 		var guts = "/doc/get_short_doc.cfm?fld=" + f;
-		$("<iframe src='" + guts + "' id='dialog' class='popupDialog' style='width:600px;height:400px;'></iframe>").dialog({
+		$("<iframe src='" + guts + "' id='dialog' class='popupDialog' style='width:800px;height:600px;'></iframe>").dialog({
 			autoOpen: true,
 			closeOnEscape: true,
 			height: 'auto',
 			modal: true,
 			position: ['center', 'center'],
 			title: 'Documentation',
-				width:600,
-	 			height:400,
+				width:800,
+	 			height:600,
 			close: function() {
 				$( this ).remove();
 			}
-		}).width(600-10).height(400-10);
+		}).width(800-10).height(600-10);
 		$(window).resize(function() {
 			$(".ui-dialog-content").dialog("option", "position", ['center', 'center']);
 		});
